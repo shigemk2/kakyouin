5c9a99d200c9a8b34fe50d998c5a87aba7cec968
shigemk2 <i.am.shige@gmail.com>
2014-07-02 00:17:36 +0900

    Add list

--- kakyouin.scala
+++ kakyouin.scala
@@ -3,12 +3,16 @@ import scala.io.Source
 object kakyouin {
   def main(args: Array[String]) {
     var source = Source.fromFile("test.rero")
+    var list = List.empty[Char]
     var j = 0
     source.foreach( { i =>
-      println(i)
-      println(j)
+      list = i :: list
+      // println(i)
+      // println(j)
       j += 1
     })
+    println(j)
+    println(list)
     source.close
   }
 }
