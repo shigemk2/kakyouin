f2d26e87248eae18688a9c0c071180193bb44080
shigemk2 <i.am.shige@gmail.com>
2014-07-01 23:29:58 +0900

    Add increment

--- kakyouin.scala
+++ kakyouin.scala
@@ -3,8 +3,11 @@ import scala.io.Source
 object kakyouin {
   def main(args: Array[String]) {
     var source = Source.fromFile("test.rero")
+    var j = 0
     source.foreach( { i =>
       println(i)
+      println(j)
+      j += 1
     })
     source.close
   }
