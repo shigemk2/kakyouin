a90d3e6e778547ade37d3ca79e7d9c2d5805b06a
shigemk2 <i.am.shige@gmail.com>
2014-07-02 00:31:39 +0900

    String rero

--- kakyouin.scala
+++ kakyouin.scala
@@ -4,9 +4,15 @@ object kakyouin {
   def main(args: Array[String]) {
     var source = Source.fromFile("test.rero")
     var list = List.empty[Char]
+    var rero = ""
     var j = 0
     source.foreach( { i =>
-      list = i :: list
+      if (i == " ") {
+        list = i :: list
+        rero = ""
+      } else {
+        rero += i
+      }
       // println(i)
       // println(j)
       j += 1
