0c2bce540b8c811b33347a6f6a9351c05f035c61
shigemk2 <i.am.shige@gmail.com>
2014-07-02 01:29:34 +0900

    Create array

--- kakyouin.scala
+++ kakyouin.scala
@@ -3,13 +3,12 @@ import scala.io.Source
 object kakyouin {
   def main(args: Array[String]) {
     var source = Source.fromFile("test.rero")
-    var list = List.empty[Char]
     var rero = ""
     source.foreach( { i =>
       rero += i
     })
-    println(rero)
-    println(rero.split(" "))
+    var array = rero.split("[\\s]+")
+    array.foreach(println)
     source.close
   }
 }
