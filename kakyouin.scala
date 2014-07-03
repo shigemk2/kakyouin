6e49d3a870edbe2a45acc55d88a4c30c7c978f46
shigemk2 <i.am.shige@gmail.com>
2014-07-03 21:23:59 +0900

    Add variables

--- kakyouin.scala
+++ kakyouin.scala
@@ -4,6 +4,11 @@ object kakyouin {
   def main(args: Array[String]) {
     var source = Source.fromFile("test.rero")
     var rero = ""
+    var mem = new Array[Int](30000)
+    var curmem = 0
+    var i = 0
+    var nest = 0
+
     source.foreach( { i =>
       rero += i
     })
