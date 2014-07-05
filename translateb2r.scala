9294608b0ed9466f10e5bede225fd6fd5c710b16
shigemk2 <i.am.shige@gmail.com>
2014-07-05 15:41:39 +0900

    Add translator brainfuck to kakyouin

--- translateb2r.scala
+++ translateb2r.scala
@@ -1,9 +1,42 @@
 import java.io.PrintWriter
+import scala.io.Source
 
-object translateb2k {
+object translateb2r {
   def main(args: Array[String]) {
-    val out = new PrintWriter(args(0))
-    out.println("Hello World!")
+    var source = Source.fromFile(args(0))
+    val out = new PrintWriter(args(1))
+
+    var rero = ""
+    var i = 0
+
+    source.foreach( { i =>
+      rero += i + " "
+    })
+    var array = rero.split("[\\s]+")
+    while (i < array.length) {
+      array(i) match {
+        case "-" =>
+          out.print("レロ ")
+        case "+" =>
+          out.print("レロレロ ")
+        case "<" =>
+          out.print("レロレロレロ ")
+        case ">" =>
+          out.print("レロレロレロレロ ")
+        case "[" =>
+          out.print("レロレロレロレロレロ ")
+        case "]" =>
+          out.print("レロレロレロレロレロレロ ")
+        case "." =>
+          out.print("レ ")
+        case "," =>
+          out.print("ロ ")
+        case _ =>
+      }
+
+      i += 1
+    }
+    source.close
     out.close
   }
 }
