abd25e9c44e88ea9adb273b80a3f4a991395af92
shigemk2 <i.am.shige@gmail.com>
2014-07-29 13:47:57 +0900

    Add BF->Scala translater base

---
+++ bftranslate.scala
@@ -0,0 +1,54 @@
+import java.io.PrintWriter
+import scala.io.Source
+
+object bftranslate {
+
+  def main(args: Array[String]) {
+    var source = Source.fromFile(args(0))
+    var out = new PrintWriter(args(1))
+    var i = 0
+    var bf = ""
+
+    out.print("object main {\n")
+    out.print("def main(args: Array[String]) {\n")
+    out.print("var mem = new Array[Int](30000)\n")
+    out.print("var r = 0\n")
+
+    source.foreach( { i =>
+       bf += i + " "
+    })
+
+    val array = bf.split("[\\s]+")
+    array.foreach( { i =>
+       println(i)
+    })
+
+    while (i < array.length) {
+      array(i) match {
+        case "+" =>
+            out.print("mem(r) += 1 // +\n")
+        case "-" =>
+            out.print("mem(r) -= 1 // -\n")
+        case ">" =>
+            out.print("r += 1 // > \n")
+        case "<" =>
+            out.print("r -= 1 // < \n")
+        case "[" =>
+            out.print("while (mem(r) > 0) { // [\n")
+        case "]" =>
+            out.print("} // ]\n")
+        case "." =>
+            out.print("print(mem(r).asInstanceOf[Char]) // .\n")
+        case "," =>
+            out.print("mem(r) = Console.in.read // ,\n")
+        }
+        i += 1
+    }
+
+    out.print("}\n");
+    out.print("}\n");
+
+    source.close
+    out.close
+  }
+}
